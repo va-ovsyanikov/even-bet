c2ee2edb76016d59c4e879eee49483a31e0dd241
va-ovsyanikov <va.ovsyanikov@gmail.com>
2024-02-27 13:19:45 +0300

    Redirect for 401 errors

--- src/services/api.services.ts
+++ src/services/api.services.ts
@@ -1,12 +1,22 @@
 import axios from 'axios'
+import router from '@/router'
 import { useAuthStore } from '@/stores'
 import type { IAuthParams, IRequest, IGameIdParams } from '@/interfaces'
-import { ELocalStoragesName } from '@/enums'
-import { getLocalStorage } from '@/utils/functions.utils'
+import { ELocalStoragesName, EPageName } from '@/enums'
+import { getLocalStorage, removeLocalStorage } from '@/utils/functions.utils'
 
 const baseURL = 'https://poker.evenbetpoker.com/api/web'
 axios.defaults.baseURL = baseURL
 axios.defaults.withCredentials = false
+axios.interceptors.response.use(undefined, (err) => {
+  if (err.response.status === 401) {
+    removeLocalStorage('token')
+    removeLocalStorage('life-time')
+    removeLocalStorage('refresh-token')
+    router.push({ name: EPageName.auth })
+  }
+  return Promise.reject(err)
+})
 
 export const request = async (data: IRequest<IAuthParams | IGameIdParams>): Promise<any> => {
   const date: number = getLocalStorage(ELocalStoragesName.lifeTime)
